21c5a8567a5fc7a765801a2338d4306b136e0c3e
Donatos Stavropoulos <dostavro@gmail.com>
2014-04-26 13:52:36 +0300

    Added backtrace when logging errors

--- lib/omf_common/thin/logging.rb
+++ lib/omf_common/thin/logging.rb
@@ -1,18 +1,18 @@
 require 'omf_common/lobject'
 
 module Thin
-  # Overwrite thin's logging mix-in to work more nicely 
+  # Overwrite thin's logging mix-in to work more nicely
   # with log4r
   #
   module Logging
     class << self
       attr_writer :trace, :debug, :silent
-      
+
       def trace?;  !@silent && @trace  end
       def debug?;  !@silent && @debug  end
       def silent?;  @silent            end
     end
-    
+
     # # Global silencer methods
     # def silent
       # Logging.silent?
@@ -20,22 +20,22 @@ module Thin
     # def silent=(value)
       # Logging.silent = value
     # end
-    
+
     # Log a message to the console
     def log(msg)
       (@logger ||= OMF::Common::LObject.new(self.class)).info(msg)
     end
     module_function :log
     public :log
-    
+
     # Log a message to the console if tracing is activated
     def trace(msg=nil)
-      return unless msg      
+      return unless msg
       (@logger ||= OMF::Common::LObject.new(self.class)).debug(msg)
     end
     module_function :trace
     public :trace
-    
+
     # Log a message to the console if debugging is activated
     def debug(msg=nil)
       return unless msg
@@ -48,7 +48,7 @@ module Thin
     def log_error(msg, e=nil)
       #log_msg = msg + ": #{e}\n\t" + e.backtrace.join("\n\t") + "\n" if e
       #Logging.log_msg(log_msg, Logger::ERROR)
-      (@logger ||= OMF::Common::LObject.new(self.class)).error(msg, e)
+      (@logger ||= OMF::Common::LObject.new(self.class)).error(msg, e.backtrace.join("\n\t"))
     end
     module_function :log_error
     public :log_error
