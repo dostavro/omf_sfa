c867c7c67e4618d604c27be857d6a4c8197e9d4e
ardadouk <ardadouk@gmail.com>
2015-05-22 17:26:46 +0300

    now that resource_associations exists /resources/accounts does not require a certficate

--- lib/omf-sfa/am/am-rest/resource_handler.rb
+++ lib/omf-sfa/am/am-rest/resource_handler.rb
@@ -46,10 +46,7 @@ module OMF::SFA::AM::Rest
         opts[:path] = opts[:req].path.split('/')[0 .. -2].join('/')
         if descr[:name].nil? && descr[:uuid].nil?
           descr[:account_id] = @am_manager.get_scheduler.get_nil_account.id if (resource_uri == 'nodes' || resource_uri == 'channels')
-          if resource_uri == 'accounts'
-            raise NotAuthorizedException, "User not found, please attach user certificates for this request." if authenticator.user.nil?
-            resource = @am_manager.find_all_accounts(authenticator)
-          elsif resource_uri == 'leases'
+          if resource_uri == 'leases'
             resource =  @am_manager.find_all_leases(nil, ["pending", "accepted", "active"], authenticator)
           else
             resource =  @am_manager.find_all_resources(descr, resource_type, authenticator)
