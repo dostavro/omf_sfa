ad57434bc19ef381a74afb8b850ce729f2938d59
ardadouk <ardadouk@gmail.com>
2015-11-23 13:39:14 +0200

    fixed link interface loop in to_hash and to_hash_brief

--- lib/omf-sfa/models/link.rb
+++ lib/omf-sfa/models/link.rb
@@ -22,5 +22,44 @@ module OMF::SFA::Model
     sfa :component_manager_id, :disabled => :true
 
     alias_method :component_manager, :component_manager_id
+
+    def self.exclude_from_json
+      sup = super
+      [].concat(sup)
+    end
+
+    def self.include_nested_attributes_to_json
+      sup = super
+      [:interfaces].concat(sup)
+    end
+
+    def to_hash
+      values.reject! { |k, v| v.nil?}
+      values[:interfaces] = []
+      self.interfaces.each do |interface|
+        values[:interfaces] << interface.to_hash_brief
+      end
+      excluded = self.class.exclude_from_json
+      values.reject! { |k, v| excluded.include?(k)}
+      values
+    end
+
+    def to_hash_brief
+      val = {}
+      val[:name] = self.name
+      val[:uuid] = self.uuid
+      val[:urn] = self.urn
+      val[:resource_type] = self.resource_type
+      val[:exclusive] = self.exclusive
+      val[:connects] = []
+      self.interfaces.each do |interface|
+        tmp = interface.node.to_hash_brief if interface.node
+        tmp = interface.openflow_switch.to_hash_brief if interface.openflow_switch
+        tmp = interface.usrp_ethernet_device.to_hash_brief if interface.usrp_ethernet_device
+        val[:connects] << tmp
+      end
+      val.reject! { |k, v| v.nil?}
+      val
+    end
   end
 end
