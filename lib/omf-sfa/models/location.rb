6dee0d15ddee151d8fa9ba630a9ce4e6469798cb
ardadouk <ardadouk@gmail.com>
2016-02-09 17:08:30 +0200

    added position 3d to location

--- lib/omf-sfa/models/location.rb
+++ lib/omf-sfa/models/location.rb
@@ -16,5 +16,12 @@ module OMF::SFA::Model
     sfa :city, :attribute => true
     sfa :longitude, :attribute => true
     sfa :latitude, :attribute => true
+    sfa :position_3d, :inline => true
+
+    def position_3d
+        return nil unless self.position_3d_x && self.position_3d_y && self.position_3d_z
+        el = "<position_3d x=\"#{self.position_3d_x}\" y=\"#{self.position_3d_y}\" z=\"#{self.position_3d_z}\"/>"
+        Nokogiri::XML(el).child
+    end
   end
 end
